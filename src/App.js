32d0e8507aa2c2c2690de04c3acef62d4a8b8eb7
yesiaw <yesiaw6@gmail.com>
2021-04-22 12:56:03 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import Menu from './Components';
 
 const App = () => {
   return (
-   <div>
-     <BrowserRouter>
+    <div>
+      <BrowserRouter>
 
-     <div>
-       <Menu/>
-     </div>
-     <Route path='/catigories/hotdishes' render={() => <h1>Горячие блюда  </h1>} />
-     <Route path='/catigories/salads' render={() =>  <h1>Салаты</h1>} />
-     <Route path='/catigories/colddishes' render={() => <h1>Холодные блюда</h1>} />
-     </BrowserRouter>
-     
-    
-    
-   </div>
+        <div>
+          <Menu />
+        </div>
+        <Route path='/catigories/hotdishes' render={() => <h1>Горячие блюда  </h1>} />
+        <Route path='/catigories/salads' render={() => <h1>Салаты</h1>} />
+        <Route path='/catigories/colddishes' render={() => <h1>Холодные блюда</h1>} />
+      </BrowserRouter>
+
+
+
+    </div>
   );
 }
 
